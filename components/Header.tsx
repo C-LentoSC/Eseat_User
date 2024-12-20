e623dbaa9e04eb366aaa432e12a5492ca421a1e7
Kalana Didulanga Koralegedara <dev.kalanadidulanga@gmail.com>
2024-12-20 22:20:17 +0530

    asdasd

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Phone } from "lucide-react";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu";
+// import { Phone } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
