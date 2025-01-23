a0e18a22f9bdd09be8916f35b5a4f4309eb41643
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-01-23 20:47:53 +0530

    update

--- components/ui/Loading.tsx
+++ components/ui/Loading.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const LoadingAnimation: React.FC = () => {
   return (
     <div className="flex justify-center items-center py-10">
-      <div className="animate-spin h-12 w-12 border-4 border-gray-300 rounded-full border-t-[#dd3170]" role="status" aria-label="loading" aria-describedby="loading-spi"></div>
+      {/* <div className="animate-spin h-12 w-12 border-4 border-gray-300 rounded-full border-t-[#dd3170]" role="status" aria-label="loading" aria-describedby="loading-spi"></div> */}
+      <img src="/assets/sltb-loading.gif" alt="loading.." className="w-40 h-40"/>
     </div>
   );
 };
