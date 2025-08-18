ae342aaa825c3e40bd62b51e627efcc61344b930
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-08-18 18:32:57 +0530

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,9 @@ export default function Header() {
                 console.error(error);
             }
         };
-        getData();
+        if(token){
+            getData();
+        }
     }, []);
 
     useEffect(() => {
