07d3f69fec2b0bd5e0250e1ad898b80af5126925
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-09-13 09:23:45 +0530

    update

--- app/payment-response/error/page.tsx
+++ app/payment-response/error/page.tsx
@@ -50,7 +50,7 @@ export default async function Page({ searchParams }: { searchParams: Record<stri
                     <span className="text-[100px] text-white">!</span>
                 </div>
                 <span className="text-[30px] font-medium">Payment Unsuccessful.</span>
-                <RetryButton link={ticketData?.paymentUrl}/>
+                <RetryButton link={ticketData?.paymentUrl ? ticketData?.paymentUrl : "/booking"} />
             </div>
         );
     } catch (error) {
